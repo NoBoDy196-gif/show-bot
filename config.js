4fc353c6eff1f5d2c29c4abae56563d696b61f38
NoBoDy196-gif <55240158+NoBoDy196-gif@users.noreply.github.com>
2020-10-18 19:14:26 +0200

    idk

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PREFIX: "$",
-  TOKEN: 'NzYyNTg2Njg5NzIwODExNTIz.X3rUMw.PtJj6npgsJmpoC4oUZuhxpCxcig',
-  DBCONNECTION: "mongodb+srv://nobodyroot:Ppaauull2004@cluster0.vrtuo.mongodb.net/<dbname>?retryWrites=true&w=majority",
-}
